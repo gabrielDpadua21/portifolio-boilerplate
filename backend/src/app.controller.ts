765c92d370ef97f9ab13415329edb63cc289fd4d
Gabriel D. Padua <gabriel.d.padua21@gmail.com>
2025-03-21 03:14:02 -0300

    feat: ✨ create ping pong api return

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get } from "@nestjs/common";
+import { Pong } from "@core";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+	constructor() {}
 
-  @Get()
-  getHello(): String {
-    return this.appService.getHello();
-  }
+	@Get("/ping")
+	ping(): String {
+		return Pong.pingPong();
+	}
 }
--- backend/src/app.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { Id } from '@core';
-
-@Injectable()
-export class AppService {
-  getHello(): String {
-    return 'Hello ID: ' + Id.generate();
-  }
-}
