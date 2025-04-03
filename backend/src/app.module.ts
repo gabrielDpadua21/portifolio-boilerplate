f9abd4d82a86b8e6931f3651809a14b1a0d6ec32
Gabriel D. Padua <gabriel.d.padua21@gmail.com>
2025-04-03 00:29:16 -0300

    chore: update imports

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
-import { DatabaseModule } from './database/database.module';
-import { ProjectsModule } from './projects/projects.module';
-import { TechsModule } from './techs/techs.module';
+import { DatabaseModule } from "./database/database.module";
+import { ProjectsModule } from "./projects/projects.module";
+import { TechsModule } from "./techs/techs.module";
 
 @Module({
 	imports: [DatabaseModule, ProjectsModule, TechsModule],
