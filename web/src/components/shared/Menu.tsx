4f37d2d802c35e5a54d38975b883b85204be0bdc
Gabriel D. Padua <gabriel.d.padua21@gmail.com>
2025-04-20 15:28:49 -0300

    feat: ✨ menu page

--- web/src/components/shared/Menu.tsx
+++ web/src/components/shared/Menu.tsx
@@ -1,12 +1,16 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Menu() {
+	const path = usePathname();
+
 	return (
 		<nav className="flex gap-6">
-			<MenuItem href="/" selected={true}>
+			<MenuItem href="/" selected={path === "/"}>
 				Home
 			</MenuItem>
-			<MenuItem href="/projeto/1" selected={false}>
+			<MenuItem href="/projects/1" selected={path.startsWith("/projects")}>
 				Projeto
 			</MenuItem>
 			<MenuItem href="/whatsapp" selected={false} newTab={true}>
@@ -24,7 +28,14 @@ function MenuItem(props: {
 }) {
 	return (
 		<Link href={props.href} target={props.newTab ? "_blank" : "_self"}>
-			{props.children}
+			<span
+				className={`
+                    flex items-center gap-2 text-sm border-red-600 hover:text-white
+                    ${props.selected ? "border-b-4 text-white" : "text-zinc-300"}
+                `}
+			>
+				{props.children}
+			</span>
 		</Link>
 	);
 }
